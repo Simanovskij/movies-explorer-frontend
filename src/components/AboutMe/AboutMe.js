812fed391c7c1f06d7b710ace53d8f40370f6818
Beiobi <a.simanovskij@gmail.com>
2021-07-12 17:49:32 +0300

    feat: добавить аватар

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,4 +1,5 @@
 import './AboutMe.css';
+import avatar from '../../images/ava.jpg';
 
 function AboutMe() {
   return (
@@ -9,14 +10,22 @@ function AboutMe() {
           <h3 className='about-me__name'>Андрей Башмаков</h3>
           <p className='about-me__career'>Фронтенд-разработчик, 33 года</p>
           <p className='about-me__description'>
-            Живу и работаю в Москве. Учился на журфаке в МГГУ им. М. А. Шолохова, а затем с 2010-го
-            года занимался эквайрингом в{' '}
+            Живу и работаю в Москве. Учился на журфаке в{' '}
+            <a href='https://mggu-sh.ru/ru'>МГГУ им. М. А. Шолохова</a>, а затем с 2010-го года
+            занимался эквайрингом в{' '}
             <a href='https://www.zenit.ru/' className='about-me__link'>
-              Банке Зенит
-            </a>
+              Банк Зенит.
+            </a>{' '}
+            Счастливо женат, есть совместный кот. Люблю бегать и коллекционировать ретро видеоигры.
           </p>
+          <a className='about-me__social-link' href='https://github.com/'>
+            Github
+          </a>
+          <a className='about-me__social-link' href='https://github.com/'>
+            Github
+          </a>
         </div>
-        <img className='about-me__photo' />
+        <img className='about-me__photo' alt='Фото автора' src={avatar} />
       </div>
     </section>
   );
