31c2e4b1044df11db2947abd46d8dbe13b3f9713
Beiobi <a.simanovskij@gmail.com>
2021-07-29 16:19:29 +0300

    feat: переделать Preloader через Suspense для SavedMovies

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,12 +1,18 @@
 import './SavedMovies.css';
+import { lazy, Suspense } from 'react';
 import Searchform from '../SearchForm/SearchForm';
-import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import Preloader from '../Preloader/Preloader';
 
 function SavedMovies(props) {
+  const MoviesCardList = lazy(
+    () => new Promise((resolve) => setTimeout(() => resolve(import('../MoviesCardList/MoviesCardList')), 1000)),
+  );
   return (
     <main className='saved-movies'>
       <Searchform />
-      <MoviesCardList pathname={props.pathname} />
+      <Suspense fallback={<Preloader />}>
+        <MoviesCardList pathname={props.pathname} />
+      < /Suspense>
     </main>
   );
 }
